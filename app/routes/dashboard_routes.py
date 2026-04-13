2e00078099e771d32046df2e53b11428b8da41e7
venkat nikhil vallamsetty <venkatnikhil616@gmail.com>
2026-04-13 12:31:13 +0530

    Create dashboard_routes.py

---
+++ app/routes/dashboard_routes.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template
+from database.crud import get_recent_logs, get_attack_stats
+                                                        dashboard_bp = Blueprint(
+    "dashboard",
+    __name__,
+    template_folder="../../dashboard/templates",
+    static_folder="../../dashboard/static"              )
+
+
+@dashboard_bp.route("/")
+def dashboard():
+    logs = get_recent_logs()
+    stats = get_attack_stats()
+
+    return render_template(
+        "index.html",
+        logs=logs,
+        stats=stats
+    )
